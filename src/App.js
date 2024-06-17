cc79dbf6c2b8fcd78a3a4d955b83fe419cfe7692
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-17 21:58:16 +0200

    create Counter component and USE context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useEffect, useState, createContext, use } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export const NumberContext = createContext(1);
+
+function Counter() {
+  const counter = use(NumberContext);
+
+  return <div>
+    <p>Number from context: {counter}</p>
+  </div>
+}
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  console.log('run inside comp');
+
+  useEffect(() => {
+    console.log('run inside useEffect');
+  }, []);
+
+  const handleOnClick = () => setCounter(c => c + 1)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleOnClick}>click {counter}</button>
+        <NumberContext.Provider value={counter}>
+          <Counter />
+        </NumberContext.Provider>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
