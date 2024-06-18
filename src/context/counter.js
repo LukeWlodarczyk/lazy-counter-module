ddb81f5cf66aa4a4a3bb6320efffc27f15c54294
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-18 15:39:18 +0200

    add useReducer instead of useState

--- src/context/counter.js
+++ src/context/counter.js
@@ -1,13 +1,32 @@
-import { useState, createContext, use } from 'react';
+import { useReducer, createContext, use } from 'react';
 
 const CounterContext = createContext(1);
 
-export const CounterProvider = ({ children }) => {
-  const [counter, setCounter] = useState(0);
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'increment': 
+      return state + 1
+    case 'decrement': 
+      return state - 1
+    case 'reset': 
+      return 0
+    default: 
+      return state;
+  }
+}
 
-  const increment = () => setCounter(counter => counter + 1);
-  const decrement = () => setCounter(counter => counter - 1);
-  const reset = () => setCounter(0);
+export const CounterProvider = ({ children }) => {
+//   const [counter, setCounter] = useState(0);
+
+//   const increment = () => setCounter(counter => counter + 1);
+//   const decrement = () => setCounter(counter => counter - 1);
+//   const reset = () => setCounter(0);
+
+  const [counter, dispatch] = useReducer(reducer, 0);
+
+  const increment = () => dispatch({ type: 'increment' });
+  const decrement = () => dispatch({ type: 'decrement' });
+  const reset = () => dispatch({ type: 'reset' });
   
   return (
     <CounterContext.Provider value={{ counter, increment, decrement, reset }}>
