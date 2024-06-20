5d528deabab9b0dfdcdf1c6be697e1e7f82898f2
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-20 22:03:18 +0200

    add code block

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 
-import LazyCounterModule from './modules/Counter';
+import LazyCounter from './modules/Counter';
 
 function App() {
   const [isCounterVisible, setCounterVisibility] = useState(false);
@@ -12,8 +12,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Lazy Loaded Counter</h1>
-        <button onClick={toggleCounter}>Toggle Counter</button>
-        {isCounterVisible && <LazyCounterModule defaultValue={1}/>}
+        <code className='App-code'>
+          {`<LazyCounter defaultValue={1} />`}
+        </code>
+        <button className="App-btn" onClick={toggleCounter}>Toggle Counter</button>
+        {isCounterVisible && <LazyCounter defaultValue={1} />}
       </header>
     </div>
   );
