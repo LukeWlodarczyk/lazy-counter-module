b0f4ef2be04b8ae95a62630bb0309d372564fee9
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-20 22:00:49 +0200

    add separate loader directory

---
+++ src/modules/Counter/Loader/Loader.js
@@ -0,0 +1,6 @@
+import logo from './logo.svg';
+import styles from  './Loader.module.css';
+
+const Loader = () => <img src={logo} className={styles.loader} alt="loading" />;
+
+export default Loader;
--- src/modules/Counter/loader.js
+++
@@ -1,5 +0,0 @@
-import logo from '../../logo.svg';
-
-const Loader = () => <img src={logo} className="App-logo" alt="logo" />;
-
-export default Loader;
